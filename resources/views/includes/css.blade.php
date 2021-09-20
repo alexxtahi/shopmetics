e2a5739799d700caed45be52f82ec25caa8c4fa3
AlexTAHI <62557152+AlexTAHI@users.noreply.github.com>
2021-09-20 20:51:41 +0000

    Découpage du template

---
+++ resources/views/includes/css.blade.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="{{ asset('fashi/css/bootstrap.min.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('fashi/css/font-awesome.min.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('fashi/css/themify-icons.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('fashi/css/elegant-icons.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('fashi/css/owl.carousel.min.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('fashi/css/nice-select.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('fashi/css/jquery-ui.min.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('fashi/css/slicknav.min.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('fashi/css/style.css') }}" type="text/css">
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Fashi Template">
+    <meta name="keywords" content="Fashi, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Shopmetics | Accueil</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <!-- Css Styles -->
+    @include('includes.css')
+</head>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+    <!-- Header Section Begin -->
+    @include('includes.header')
+    <!-- Header End -->
+
+    <!-- Hero Section Begin -->
+    @include('layouts.hero')
+    <!-- Hero Section End -->
+
+    <!-- Banner Section Begin -->
+    @include('layouts.banner')
+    <!-- Banner Section End -->
+
+    <!-- Partner Logo Section Begin -->
+    @include('includes.content')
+    <!-- Partner Logo Section End -->
+
+    <!-- Footer Section Begin -->
+    @include('includes.footer')
+    <!-- Footer Section End -->
+
+    <!-- Js Plugins -->
+    @include('includes.js')
+
+</body>
+
+</html>
