ef18aad0f3a0e08a9bf7627e47edbb53a768eb7d
HackerMan018 <bouadoutanoh9@gmail.com>
2021-09-21 13:00:14 +0000

    mise a jour du header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -4,7 +4,7 @@
                 <div class="ht-left">
                     <div class="mail-service">
                         <i class=" fa fa-envelope"></i>
-                        hello.colorlib@gmail.com
+                        shopmetics@gmail.com
                     </div>
                     <div class="phone-service">
                         <i class=" fa fa-phone"></i>
@@ -12,13 +12,15 @@
                     </div>
                 </div>
                 <div class="ht-right">
-                    <a href="#" class="login-panel"><i class="fa fa-user"></i>Login</a>
+                    <a href="#" class="login-panel"><i class="fa fa-user"></i>Connexion</a>
                     <div class="lan-selector">
                         <select class="language_drop" name="countries" id="countries" style="width:300px;">
                             <option value='yt' data-image="{{ asset('fashi/img/flag-1.jpg') }}" data-imagecss="flag yt"
                                 data-title="English">English</option>
                             <option value='yu' data-image="{{ asset('fashi/img/flag-2.jpg') }}" data-imagecss="flag yu"
                                 data-title="Bangladesh">German </option>
+                                <option value='yu' data-image="{{ asset('fashi/img/flag-2.jpg') }}" data-imagecss="flag yu"
+                                data-title="Bangladesh">Francais </option>
                         </select>
                     </div>
                     <div class="top-social">
@@ -42,9 +44,9 @@
                     </div>
                     <div class="col-lg-7 col-md-7">
                         <div class="advanced-search">
-                            <button type="button" class="category-btn">All Categories</button>
+                            <button type="button" class="category-btn">Catégories</button>
                             <div class="input-group">
-                                <input type="text" placeholder="What do you need?">
+                                <input type="text" placeholder="Que cherchez vous ? ">
                                 <button type="button"><i class="ti-search"></i></button>
                             </div>
                         </div>
@@ -54,16 +56,17 @@
                             <li class="heart-icon">
                                 <a href="#">
                                     <i class="icon_heart_alt"></i>
-                                    <span>1</span>
+                                    <!-- <span>1</span> -->
                                 </a>
                             </li>
                             <li class="cart-icon">
                                 <a href="#">
                                     <i class="icon_bag_alt"></i>
-                                    <span>3</span>
+                                   <!--  <span>3</span> -->
                                 </a>
                                 <div class="cart-hover">
                                     <div class="select-items">
+                                        <!--
                                         <table>
                                             <tbody>
                                                 <tr>
@@ -92,18 +95,18 @@
                                                 </tr>
                                             </tbody>
                                         </table>
+                                    -->
                                     </div>
                                     <div class="select-total">
                                         <span>total:</span>
-                                        <h5>$120.00</h5>
+                                        <!-- <h5>$120.00</h5> -->
                                     </div>
                                     <div class="select-button">
-                                        <a href="#" class="primary-btn view-card">VIEW CARD</a>
-                                        <a href="#" class="primary-btn checkout-btn">CHECK OUT</a>
+                                        <a href="#" class="primary-btn view-card">Valider la commande</a>
                                     </div>
                                 </div>
                             </li>
-                            <li class="cart-price">$150.00</li>
+                            <!-- <li class="cart-price">$150.00</li> -->
                         </ul>
                     </div>
                 </div>
@@ -112,42 +115,25 @@
         <div class="nav-item">
             <div class="container">
                 <div class="nav-depart">
-                    <div class="depart-btn">
-                        <i class="ti-menu"></i>
-                        <span>All departments</span>
-                        <ul class="depart-hover">
-                            <li class="active"><a href="#">Women’s Clothing</a></li>
-                            <li><a href="#">Men’s Clothing</a></li>
-                            <li><a href="#">Underwear</a></li>
-                            <li><a href="#">Kid's Clothing</a></li>
-                            <li><a href="#">Brand Fashion</a></li>
-                            <li><a href="#">Accessories/Shoes</a></li>
-                            <li><a href="#">Luxury Brands</a></li>
-                            <li><a href="#">Brand Outdoor Apparel</a></li>
-                        </ul>
-                    </div>
-                </div>
                 <nav class="nav-menu mobile-menu">
                     <ul>
-                        <li class="active"><a href="./index.html">Home</a></li>
+                        <li class="active"><a href="./index.html">Acceuil</a></li>
                         <li><a href="./shop.html">Shop</a></li>
                         <li><a href="#">Collection</a>
                             <ul class="dropdown">
-                                <li><a href="#">Men's</a></li>
-                                <li><a href="#">Women's</a></li>
-                                <li><a href="#">Kid's</a></li>
+                                <li><a href="#">Beauté & soin</a></li>
+                                <li><a href="#">Maquillage</a></li>
+                                <li><a href="#">Santé</a></li>
                             </ul>
                         </li>
                         <li><a href="./blog.html">Blog</a></li>
                         <li><a href="./contact.html">Contact</a></li>
                         <li><a href="#">Pages</a>
                             <ul class="dropdown">
-                                <li><a href="./blog-details.html">Blog Details</a></li>
-                                <li><a href="./shopping-cart.html">Shopping Cart</a></li>
-                                <li><a href="./check-out.html">Checkout</a></li>
+                                <li><a href="./check-out.html">Panier</a></li>
                                 <li><a href="./faq.html">Faq</a></li>
-                                <li><a href="./register.html">Register</a></li>
-                                <li><a href="./login.html">Login</a></li>
+                                <li><a href="./register.html">Inscription</a></li>
+                                <li><a href="./login.html">Connexion</a></li>
                             </ul>
                         </li>
                     </ul>
